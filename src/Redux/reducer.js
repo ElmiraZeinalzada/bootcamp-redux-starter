e0425605d6dc4f8e73eca1f104efb9bdced40471
ElmiraZeinalzada <114818065+ElmiraZeinalzada@users.noreply.github.com>
2022-12-01 14:44:16 +0400

    2 files modified, 1 added

---
+++ src/Redux/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  sebet: [],
+  mal: [],
+};
+function reducer(state=initialState,action){
+  switch(action.type){
+    case ''
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { store } from './redux/store';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
