581ec33ab91437aa77e9ce321aaebe575835fcf9
ElmiraZeinalzada <114818065+ElmiraZeinalzada@users.noreply.github.com>
2022-12-01 15:00:10 +0400

    2 files modified, 1 added

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,6 +1,17 @@
 const initialState = {
-  sebet: [],
-  mal: [],
+  state={
+  cart: [],
+  good: [],
+  }
+};
+addCart = () => {
+  store.dispatch({
+    type: 'ADD_TO_CART',
+    payload: {
+      todo: this.state.cart,
+    },
+  });
+  this.setState({ cart: '' });
 };
 function reducer(state = initialState, action) {
   switch (action.type) {
--- src/components/GoodsItem.js
+++ src/components/GoodsItem.js
@@ -1,6 +1,5 @@
-import React, { PureComponent } from "react";
-import "../styles.css";
-
+import React, { PureComponent } from 'react';
+import '../styles.css';
 
 class GoodsItem extends PureComponent {
   render() {
@@ -10,11 +9,17 @@ class GoodsItem extends PureComponent {
       <div className="goods-item" key={id}>
         <h3 className="goods-item__title">{title}</h3>
         <p className="goods-item__price">
-          <span className="goods-item__price-value goods-item__price-value_old">{price*1.2}.00$ </span>
-          <span className="goods-item__price-value goods-item__price-value_new">{price}.00$</span>
+          <span className="goods-item__price-value goods-item__price-value_old">
+            {price * 1.2}.00${' '}
+          </span>
+          <span className="goods-item__price-value goods-item__price-value_new">
+            {price}.00$
+          </span>
         </p>
         <p className="goods-item__description">{description}</p>
-        <button className="goods-item__add-to-card">Add to cart</button>
+        <button onClick={this.addCart} className="goods-item__add-to-card">
+          Add to cart
+        </button>
       </div>
     );
   }
