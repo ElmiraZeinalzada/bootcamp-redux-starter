e04500bdc20e6361a62825e93673a404715b64bd
ElmiraZeinalzada <114818065+ElmiraZeinalzada@users.noreply.github.com>
2022-12-05 12:50:08 +0400

    1 file modified

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -4,15 +4,7 @@ const initialState = {
   good: [],
   }
 };
-addCart = () => {
-  store.dispatch({
-    type: 'ADD_TO_CART',
-    payload: {
-      todo: this.state.cart,
-    },
-  });
-  this.setState({ cart: '' });
-};
+
 function reducer(state = initialState, action) {
   switch (action.type) {
     case 'DELETE FROM CART': {
